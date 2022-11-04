4f053a74f9fba7b9191cf0e9f27087c7374022ad
harsh.yadav3 <harsh.yadav@bitcs.in>
2022-11-04 14:50:10 +0530

    vc

--- backend/dist/database/index.d.ts
+++ backend/dist/database/index.d.ts
@@ -2,6 +2,6 @@ import { Bike } from "src/entity/bike.entity";
 import { Reservation } from "src/entity/reservation.entity";
 import { Review } from "src/entity/review.entity";
 import { User } from "src/entity/user.entity";
-declare const entities: (typeof User | typeof Bike | typeof Review | typeof Reservation)[];
+declare const entities: (typeof Review | typeof Bike | typeof User | typeof Reservation)[];
 export { User };
 export default entities;
